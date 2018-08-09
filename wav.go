31a402c022ba16e3573f0c70f30d99ef66dd9dfa
but80 <mersenne.sister@gmail.com>
2018-08-09 23:21:05 +0900

    クリッピング警告

--- wav.go
+++ wav.go
@@ -2,6 +2,7 @@ package voispire
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 
@@ -42,7 +43,19 @@ func saveWav(filename string, fs int, data []float64) error {
 		return errors.Wrap(err, "出力音声ファイルのオープンに失敗しました")
 	}
 	enc := wav.NewEncoder(out, fs, 16, 1, 1)
+	noClip := true
 	for _, v := range data {
+		if v < -1.0 || 1.0 < v {
+			if v < -1.0 {
+				v = -1.0
+			} else if 1.0 < v {
+				v = 1.0
+			}
+			if noClip {
+				log.Print("warn: クリッピングが発生しました")
+				noClip = false
+			}
+		}
 		if err := enc.WriteFrame(uint16(v * 32767)); err != nil {
 			return errors.Wrap(err, "出力音声ファイルの書き込みに失敗しました")
 		}
