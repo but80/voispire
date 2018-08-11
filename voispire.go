66525083e02a12b6390a026f76025b706ec860dd
but80 <mersenne.sister@gmail.com>
2018-08-11 22:50:35 +0900

    出力のPortAudioストリーム化

--- voispire.go
+++ voispire.go
@@ -3,10 +3,51 @@ package voispire
 import (
 	"log"
 	"math"
+	"time"
 
 	"github.com/but80/voispire/internal/world"
+	"github.com/gordonklaus/portaudio"
 	"github.com/pkg/errors"
+	"github.com/xlab/closer"
 )
+
+func render(in chan float64) error {
+	portaudio.Initialize()
+	closer.Bind(func() {
+		portaudio.Terminate()
+	})
+
+	hostapi, err := portaudio.DefaultHostApi()
+	if err != nil {
+		return err
+	}
+	log.Printf("info: Audio device: %s\n", hostapi.DefaultOutputDevice.Name)
+	params := portaudio.HighLatencyParameters(nil, hostapi.DefaultOutputDevice)
+	stream, err := portaudio.OpenStream(params, func(out [][]float32) {
+		for i := range out[0] {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					break
+				}
+				out[0][i] = float32(v)
+				out[1][i] = float32(v)
+			default:
+				break
+			}
+		}
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("info: Sample rate: %f\n", stream.Info().SampleRate)
+	log.Printf("info: Output latency: %s\n", stream.Info().OutputLatency.String())
+
+	if err := stream.Start(); err != nil {
+		return err
+	}
+	return nil
+}
 
 const (
 	framePeriod = .005
@@ -27,19 +68,16 @@ func Demo(transpose int, infile, outfile string) error {
 	splittedCh := splitShapes(src, f0, float64(fs))
 	pitchCoef := math.Pow(2.0, float64(transpose)/12.0)
 	outCh := stretch(splittedCh, pitchCoef, 1.0)
-	result := make([]float64, len(src))
-	i := 0
-	for i < len(result) {
-		v, ok := <-outCh
-		if !ok {
-			break
-		}
-		result[i] = v
-		i++
+	if err := render(outCh); err != nil {
+		return errors.Wrap(err, "出力ストリームのオープンに失敗しました")
 	}
 
-	log.Print("info: 保存中...")
-	saveWav(outfile, fs, result)
-	log.Print("info: 完了")
+	for {
+		time.Sleep(time.Second)
+	}
+
+	// log.Print("info: 保存中...")
+	// saveWav(outfile, fs, result)
+	// log.Print("info: 完了")
 	return nil
 }
