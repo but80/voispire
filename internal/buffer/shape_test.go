c318abc2dfa50e0c0b6cd86f0f3e7f12d0874cf6
but80 <mersenne.sister@gmail.com>
2018-08-16 12:17:53 +0900

    Add test

--- internal/buffer/shape_test.go
+++ internal/buffer/shape_test.go
@@ -12,18 +12,37 @@ func roundf(v, p float64) float64 {
 }
 
 func TestShape_getLagrange(t *testing.T) {
-	sh := MakeShape([]float64{
+	sh1 := MakeShape([]float64{
 		-.04, // phase=0.0/8.0
 		.00,  // phase=2.0/8.0
 		.04,  // phase=4.0/8.0
 		.16,  // phase=6.0/8.0
 	})
-	assert.Equal(t, -.04, roundf(sh.getLagrange(0.0/8.0), .001))
-	assert.Equal(t, -.02, roundf(sh.getLagrange(1.0/8.0), .001)) // linear
-	assert.Equal(t, .00, roundf(sh.getLagrange(2.0/8.0), .001))
-	assert.Equal(t, .02, roundf(sh.getLagrange(3.0/8.0), .001)) // linear
-	assert.Equal(t, .04, roundf(sh.getLagrange(4.0/8.0), .001))
-	assert.Equal(t, .09, roundf(sh.getLagrange(5.0/8.0), .001)) // lagrange
-	assert.Equal(t, .16, roundf(sh.getLagrange(6.0/8.0), .001))
-	assert.Equal(t, .16, roundf(sh.getLagrange(7.0/8.0), .001)) // keep last sample
+	assert.Equal(t, -.04, roundf(sh1.getLagrange(0.0/8.0), .001))
+	assert.Equal(t, -.02, roundf(sh1.getLagrange(1.0/8.0), .001)) // linear
+	assert.Equal(t, .00, roundf(sh1.getLagrange(2.0/8.0), .001))
+	assert.Equal(t, .02, roundf(sh1.getLagrange(3.0/8.0), .001)) // linear
+	assert.Equal(t, .04, roundf(sh1.getLagrange(4.0/8.0), .001))
+	assert.Equal(t, .09, roundf(sh1.getLagrange(5.0/8.0), .001)) // lagrange
+	assert.Equal(t, .16, roundf(sh1.getLagrange(6.0/8.0), .001))
+	assert.Equal(t, .16, roundf(sh1.getLagrange(7.0/8.0), .001)) // keep last sample
+	//
+	sh2 := MakeShapeTrimmed([]float64{
+		-.16, // phase=-4.0/8.0
+		-.04, // phase=-2.0/8.0
+		.00,  // phase=0.0/8.0
+		.04,  // phase=2.0/8.0
+		.16,  // phase=4.0/8.0
+		.36,  // phase=6.0/8.0
+		.64,  // phase=8.0/8.0
+	}, 2, 2+4)
+	assert.Equal(t, .00, roundf(sh2.getLagrange(0.0/8.0), .001))
+	assert.Equal(t, .02, roundf(sh2.getLagrange(1.0/8.0), .001)) // ?
+	assert.Equal(t, .04, roundf(sh2.getLagrange(2.0/8.0), .001))
+	assert.Equal(t, .09, roundf(sh2.getLagrange(3.0/8.0), .001)) // lagrange
+	assert.Equal(t, .16, roundf(sh2.getLagrange(4.0/8.0), .001))
+	assert.Equal(t, .25, roundf(sh2.getLagrange(5.0/8.0), .001)) // lagrange
+	assert.Equal(t, .36, roundf(sh2.getLagrange(6.0/8.0), .001))
+	assert.Equal(t, .49, roundf(sh2.getLagrange(7.0/8.0), .001)) // lagrange
+	//
 }
