0566779c52bdf813eb5d089a1135433904b6bb14
but80 <mersenne.sister@gmail.com>
2019-09-25 21:57:28 +0900

    tools/vocoder.py: Add --visualize option

--- tools/vocoder.py
+++ tools/vocoder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# usage: vocoder.py [-h] [-t TRANSPOSE] [-f FORMANT] [-b BREATHINESS] [-e]
+# usage: vocoder.py [-h] [-t TRANSPOSE] [-f FORMANT] [-b BREATHINESS] [-e] [-v]
 #                   in_file [out_file]
 #
 # positional arguments:
@@ -16,19 +16,24 @@
 #   -b BREATHINESS, --breathiness BREATHINESS
 #                         breathiness boost [%]
 #   -e, --economy         use Dio instead of Harvest
+#   -v, --visualize       visualize f0, sp and ap with Sixel
 
-from pprint import pprint
 import argparse
 import numpy as np
 import soundfile as sf
 import pyworld as pw
 import simpleaudio as sa
+import matplotlib.pyplot as plt
+from pprint import pprint
+import tempfile
+import sixel
 
 parser = argparse.ArgumentParser(prog='vocoder.py')
 parser.add_argument('-t', '--transpose', type=float, default=6., help='transpose [semitones]')
 parser.add_argument('-f', '--formant', type=float, default=3., help='formant shift [semitones]')
 parser.add_argument('-b', '--breathiness', type=float, default=.3, help='breathiness boost [%%]')
 parser.add_argument('-e', '--economy', action='store_const', const=True, help='use Dio instead of Harvest')
+parser.add_argument('-v', '--visualize', action='store_const', const=True, help='visualize f0, sp and ap with Sixel')
 parser.add_argument('in_file', help='input wav file')
 parser.add_argument('out_file', nargs='?', help='output wav file (default: direct playback)')
 
@@ -62,6 +67,24 @@ def retouch_noise(ap, width, level):
         ap[i] = pow(ap[i], lerp(1., 1.-level, t))
     return ap.T
 
+def show_figure(f, log=True):
+    epsilon = 1e-8
+    plt.figure()
+    if len(f.shape) == 1:
+        plt.plot(f)
+        plt.xlim([0, len(f)])
+    elif len(f.shape) == 2:
+        if log:
+            x = np.log(f + epsilon)
+        else:
+            x = f + epsilon
+        plt.imshow(x.T, origin='lower', interpolation='none', aspect='auto', extent=(0, x.shape[0], 0, x.shape[1]))
+    with tempfile.NamedTemporaryFile(prefix='sixel-') as fd:
+        plt.savefig(fd, format='png')
+        fd.flush()
+        writer = sixel.SixelWriter()
+        writer.draw(fd.name)
+
 def main(args):
     x, fs = sf.read(args.in_file)
 
@@ -85,6 +108,10 @@ def main(args):
     sp = shift_formant(sp, args.formant)
     ap = retouch_noise(ap, .8, args.breathiness/100.)
     y = pw.synthesize(f0, sp, ap, fs, pw.default_frame_period)
+    if args.visualize:
+        show_figure(f0)
+        show_figure(sp)
+        show_figure(ap)
     if args.out_file is not None:
         sf.write(args.out_file, y, fs)
     else:
